8f3ec6a8ede8abd4400164ed7b6bbe92032925d5
肖鑫 <16514132+xiao-xin-111@user.noreply.gitee.com>
2026-07-23 17:42:09 +0800

    调整前端架构

--- chatbot-frontend/src/app/App.tsx
+++ chatbot-frontend/src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
+import './App.module.less';
 
 function App() {
     const [userInput, setUserInput] = useState('');
